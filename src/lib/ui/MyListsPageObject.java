50a8ce9204512ea71166f75211fcf58e2f8a7d48
puzigupka <57733107+puzigupka@users.noreply.github.com>
2020-07-13 18:07:01 +0300

    Ex10: Настройка работы на iOS

--- src/lib/ui/MyListsPageObject.java
+++ src/lib/ui/MyListsPageObject.java
@@ -6,8 +6,8 @@ import org.openqa.selenium.By;
 public class MyListsPageObject extends MainPageObject
 {
     public static final String
-    FOLDER_BY_NAME_TRL = "//*[@text='{FOLDER_NAME}']",
-    ARTICLE_BY_TITLE_TPL = "//*[@text='{TITLE}']";
+    FOLDER_BY_NAME_TRL = "xpath://*[@text='{FOLDER_NAME}']",
+    ARTICLE_BY_TITLE_TPL = "xpath://*[@text='{TITLE}']";
 
     private static String getFolderXpathByName(String name_of_folder)
     {
@@ -29,7 +29,7 @@ public class MyListsPageObject extends MainPageObject
     {
         String folder_name_xpath = getFolderXpathByName(name_of_folder);
         this.waitForElementAndClick(
-                By.xpath(folder_name_xpath),
+                folder_name_xpath,
                 "Cannot find folder by name" + name_of_folder,
                 5
         );
@@ -39,13 +39,13 @@ public class MyListsPageObject extends MainPageObject
     public void waitForArticleToAppearByTitle(String article_title)
     {
         String article_xpath = getFolderXpathByName(article_title);
-        this.waitForElementPresent(By.xpath(article_xpath), "Cannot find saved article by title" + article_title, 15);
+        this.waitForElementPresent(article_xpath, "Cannot find saved article by title" + article_title, 15);
     }
 
     public void waitForArticleToDisappearByTitle(String article_title)
     {
         String article_xpath = getFolderXpathByName(article_title);
-        this.waitForElementNotPresent(By.xpath(article_title), "saved article still present with title" + article_title, 15);
+        this.waitForElementNotPresent(article_title, "saved article still present with title" + article_title, 15);
     }
 
     public void swipeByArticleToDelete(String article_title)
@@ -53,7 +53,7 @@ public class MyListsPageObject extends MainPageObject
         this.waitForArticleToAppearByTitle(article_title);
         String article_xpath = getFolderXpathByName(article_title);
         this.swipeUpElementToLeft(
-                By.xpath(article_xpath),
+                article_xpath,
                 "Cannot find saved article"
 
         );
